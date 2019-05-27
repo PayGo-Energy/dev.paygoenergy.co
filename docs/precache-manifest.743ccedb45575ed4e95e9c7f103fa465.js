c319b9a8180316b3731a6c0e4f1f827c9950aa14
alxndrsn <@alxndrsn>
2019-05-27 12:56:17 +0300

    Add Github Pages site

---
+++ docs/precache-manifest.743ccedb45575ed4e95e9c7f103fa465.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9083a7693fc60570a110f10ecf4479d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41ab47df111cb4d12cf0",
+    "url": "/static/css/main.8b929ad3.chunk.css"
+  },
+  {
+    "revision": "1a9499c0e75bcac2a929",
+    "url": "/static/js/2.d2d94e41.chunk.js"
+  },
+  {
+    "revision": "41ab47df111cb4d12cf0",
+    "url": "/static/js/main.02985b15.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
