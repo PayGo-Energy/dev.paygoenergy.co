23c30c5d3ddf1f6e0d976e618c3895ccb3ed4601
Alex Anderson <alxndrsn>
2020-08-27 11:44:04 +0000

    jobs: add toggle for job advert

--- src/job-ad.js
+++ src/job-ad.js
@@ -3,6 +3,8 @@
 const jobAd =
 '# Software Engineer\n' +
 '\n' +
+'Location: Nairobi, Kenya\n' +
+'\n' +
 '[PayGo Energy](https://www.paygoenergy.co) are looking for a senior engineer to join our product-focussed, agile team to help develop and support live services in a rapidly-growing startup.\n' +
 '\n' +
 '\n' +
@@ -11,14 +13,14 @@ const jobAd =
 '* use kanban\n' +
 '* work in deliverable-focussed ~fortnightly iterations\n' +
 '* deliver incremental improvements to our users frequently\n' +
-'* continuously build and deploy to Google Cloud Platform with k8s and docker\n' +
+'* continuously build and deploy to Google Cloud Platform\n' +
 '* release multiple times per week\n' +
 '* develop with modern javascript (ES8) for the whole stack, on nodejs, react and react-native\n' +
 '* use proper industry-proven databases (PostgreSQL)\n' +
 '* actively monitor and support live environments\n' +
 '* work closely with our hardware team to integrate with hundreds of IoT devices\n' +
 '* constantly work to reduce our support load while increasing our userbase\n' +
-'* work from our quiet Nairobi office in Bishop Magua Centre (Kilimani)\n' +
+'* work <del>from our quiet Nairobi office in Bishop Magua Centre (Kilimani)</del> mostly remotely due to Covid-19 restrictions\n' +
 '\n' +
 '\n' +
 '## You\n' +
@@ -33,8 +35,6 @@ const jobAd =
 '## So\n' +
 '\n' +
 'If that sounds like you, send CV/cover letter/linkedin/github to [software-hiring@paygoenergy.co](mailto:software-hiring@paygoenergy.co) (yes, `.co`).\n' +
-'\n' +
-'If you don\'t think you have the skills or experience necessary _yet_, drop us your CV and cover letter anyway - **we\'re also hiring junior and mid-level developers**, and are always looking for enthusiastic and talented people to join our team.\n' +
 '';
 
 export default jobAd;
--- src/pages/jobs.js
+++ src/pages/jobs.js
@@ -3,12 +3,16 @@ import React from "react"
 import Layout from '../Layout';
 import Markdown from '../Markdown';
 
+import jobAd from '../job-ad';
 import noJobOpenings from '../no-job-openings.md';
+
+const gotJobs = false;
 
 export default function Jobs() {
   return (
     <Layout title="Software Jobs">
-      <Markdown source={noJobOpenings}/>
+      {gotJobs && <Markdown source={jobAd}/>}
+      {gotJobs || <Markdown source={noJobOpenings}/>}
     </Layout>
   );
 }
